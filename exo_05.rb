55537c89de3ef44bd1d8555c81172691980e7606
PaulineLabaisse <pauline.labaisse@orange.fr>
2018-09-27 11:48:36 +0200

    Fith exercice ruby

---
+++ exo_05.rb
@@ -0,0 +1,24 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10*5*11}" #calcule le temps de travail en heures
+puts "En minutes ca fait : #{10*5*11*60}" #calcule le temps de travail en minutes
+
+puts "Et en secondes?"
+
+puts 10*5*11*60*60 #est un calcule qui repond à la ligne au dessus
+
+puts "Est ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7 #est un calcule qui repond à la ligne au dessus
+
+#les #{} calculent directement les elements compreis entre
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "Ok c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts"Est-ce-que 5 est plus grand que -2 ? #{5 > -2}"
+puts"Est-ce-que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts"Est-ce-que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
