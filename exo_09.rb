b7f735fae95b2a0a87822bbfc1ad9587de5cb67c
PaulineLabaisse <pauline.labaisse@orange.fr>
2018-09-27 13:02:57 +0200

    Last exercice programme ruby

---
+++ exo_09.rb
@@ -0,0 +1,5 @@
+puts"Bonjour, quel est ton prénom ?"
+user_name = gets.chomp
+puts"Enchanté, quel est ton nom de famille ?"
+user =  gets.chomp
+puts"Contant de te connaitre " + user_name + " " + user
